facb74bf7690cb383c725d952cf84c05d227eaf2
Dan Kaiser <Tophatman100@gmail.com>
2019-05-03 15:29:33 -0500

    refactor to stateful class component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const App = (props) => (
-  <div className='container'>
-    React Hello World
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.onClickTest = this.onClickTest.bind(this);
+  }
 
+  onClickTest(event) {
+    console.log(event.target.textContent);
+  }
+
+  render() {
+    return (
+      <div onClick={this.onClickTest}>
+        React Hello World
+      </div>
+    )
+  }
+}
 export default App;
