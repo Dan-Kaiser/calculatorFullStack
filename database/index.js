97a0723cae338efa529c372465cd214f021cd179
Dan Kaiser <Tophatman100@gmail.com>
2019-05-03 16:23:16 -0500

    connect to the mongodb

--- database/index.js
+++ database/index.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const Schema = mongoose.Schema;
+
+const db = mongoose.connect(`mongodb+srv://Dan:${process.env.DBPASSWORD}@fullstackcalculatorcluster-daz5w.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log(err, 'error in mongoose.connect -- mongoose.connect');
+  } else {
+    console.log('connected to the mongo fullStackCalculator database');
+  }
+});
