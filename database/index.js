fafe9ba05e1cd740ee4506a5680ba4dccd137a8e
Dan Kaiser <Tophatman100@gmail.com>
2019-05-04 14:15:34 -0500

    connect from client to server, and server to db, can save to db

--- database/index.js
+++ database/index.js
@@ -1,11 +1,45 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const Schema = mongoose.Schema;
+//`mongodb+srv://Dan:${process.env.DBPASSWORD}@fullstackcalculatorcluster-daz5w.mongodb.net/---->Replace me for different collection name<----?retryWrites=true`
 
-const db = mongoose.connect(`mongodb+srv://Dan:${process.env.DBPASSWORD}@fullstackcalculatorcluster-daz5w.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true }, (err) => {
-  if (err) {
-    console.log(err, 'error in mongoose.connect -- mongoose.connect');
-  } else {
-    console.log('connected to the mongo fullStackCalculator database');
-  }
+let calculationSchema = new Schema({
+  resultString: String
 });
+
+let Calculation = mongoose.model('Calculation', calculationSchema);
+
+const calculationCounter = (callback) => {
+  // console.log('we in calc function');
+  Calculation.countDocuments().exec((err, count) => {
+    if (err) {
+      console.log('error in calculationCounter', err);
+      return err;
+    }
+    // console.log('no error in Calculation Function')
+    callback(count);
+  })
+}
+
+const insertResult = (val, callback) => {
+  // console.log('inside insert Result, with value', val);
+  let currCalc = new Calculation({ resultString: val });
+  currCalc.save((err, currCalc) => {
+    console.log('inside save callback')
+    if (err) return console.error(err);
+    callback(`Saved ${currCalc} to database!`);
+  });
+}
+
+const db = mongoose.connect(`mongodb+srv://Dan:${process.env.DBPASSWORD}@fullstackcalculatorcluster-daz5w.mongodb.net/test?retryWrites=true
+`, { useNewUrlParser: true }, (err) => {
+    if (err) {
+      console.log(err, 'error in mongoose.connect -- mongoose.connect');
+    } else {
+      console.log('connected to the mongo fullStackCalculator database');
+    }
+  });
+
+module.exports.calculationCounter = calculationCounter;
+module.exports.insertResult = insertResult;
+module.exports.db = db;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.port || 3000;
+const db = require('../database/index.js')
 
 app.use(express.static('../client/dist'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/test', (req, res) => res.send('Test Express Hello World!'))
+app.get('/count', (req, res) => {
+  db.calculationCounter(result => {
+    console.log('we got back a result', result);
+    res.send(`the current count = ${result}`);
+  })
+});
+app.post('/insert', (req, res) => {
+  db.insertResult('2 + 4 = 6', (results) => {
+    if (results) {
+      res.send(results);
+    } else {
+      res.send('error in insert');
+    }
+  });
+})
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
