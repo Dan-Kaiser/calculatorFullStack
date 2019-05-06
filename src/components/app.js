8ae2f1ad9d3c238e3933668f703a3706d0748852
Dan Kaiser <Tophatman100@gmail.com>
2019-05-06 16:49:12 -0500

    will auto-refersh every second

--- src/components/app.js
+++ src/components/app.js
@@ -41,7 +41,7 @@ class App extends React.Component {
 
   componentWillMount() {
     this.queryRecentResults();
-    // window.setInterval(this.queryRecentResults, 1000);
+    window.setInterval(this.queryRecentResults, 1000);
   }
 
   clearState() {
